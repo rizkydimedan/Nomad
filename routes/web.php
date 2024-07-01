463f5e6295502c3905c341a8a1f799f2b9d23fa2
rizkydimedan <rizkyanakmedan77@gmail.com>
2024-07-02 01:05:25 +0700

    Travel

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::prefix('admin')->namespace('Admin')->group( function(){
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    });
